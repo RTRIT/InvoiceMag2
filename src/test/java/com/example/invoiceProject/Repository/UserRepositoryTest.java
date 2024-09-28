4f7d434d353306b0457d618e1ee70d48cffdaff8
RTRIT <petertringuyen03@gmail.com>
2024-09-28 15:02:45 +0700

    commit test UserRepositoryTest

--- src/test/java/com/example/invoiceProject/Repository/UserRepositoryTest.java
+++ src/test/java/com/example/invoiceProject/Repository/UserRepositoryTest.java
@@ -1,5 +1,7 @@
 package com.example.invoiceProject.Repository;
 
+import com.example.invoiceProject.Model.Privilege;
+import com.example.invoiceProject.Model.Role;
 import com.example.invoiceProject.Model.User;
 import org.junit.After;
 import org.junit.Before;
@@ -12,6 +14,9 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.Assert.*;
 
 @RunWith(SpringRunner.class)
@@ -20,13 +25,50 @@ public class UserRepositoryTest {
 
     @Autowired
     UserRepository userRepository;
+    @Autowired
+    RoleRepository roleRepository;
+    @Autowired
+    PrivilegeRepository privilegeRepository;
 
     @Before
     public void setUp() throws Exception {
+
+        Privilege privilege1 = new Privilege();
+        privilege1.setPrivilegeName("Edit");
+        privilege1.setPrivilegeDesc("Edit User");
+        Privilege privilege2 = new Privilege();
+        privilege2.setPrivilegeName("Add");
+        privilege2.setPrivilegeDesc("Add User");
+        Privilege privilege3 = new Privilege();
+        privilege3.setPrivilegeName("Delete");
+        privilege3.setPrivilegeDesc("Delete User");
+        //Save privilege
+        privilegeRepository.save(privilege1);
+        privilegeRepository.save(privilege2);
+        privilegeRepository.save(privilege3);
+
+
+        List<Privilege> privilegeList = new ArrayList<>();
+        privilegeList.add(privilege1);
+        privilegeList.add(privilege2);
+        privilegeList.add(privilege3);
+
+
+        Role role = new Role();
+        role.setRoleName("USER");
+        role.setPrivileges(privilegeList);
+        roleRepository.save(role);
+
+        Role newRole = roleRepository.findByRoleName("USER");
+
         //create data for testing;
         User user = new User();
+        user.setEmail("tringuyen240503@gmail.com");
+        user.setPassword("123456789");
+        user.setRole(newRole);
 
         userRepository.save(user);
+
     }
 
     @After
