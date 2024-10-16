f62e62460b489a9cf10191fc1c845e58c4febd7c
Bluephoenix <biendien4508@gmail.com>
2024-10-16 17:16:47 +0700

    update Privilege

--- src/main/java/com/example/invoiceProject/Controller/PrivilegeController.java
+++ src/main/java/com/example/invoiceProject/Controller/PrivilegeController.java
@@ -1,35 +1,46 @@
 package com.example.invoiceProject.Controller;
 
+import com.example.invoiceProject.DTO.response.ApiResponse;
+import com.example.invoiceProject.DTO.requests.PrivilegeRequest;
+import com.example.invoiceProject.DTO.response.PrivilegeResponse;
 import com.example.invoiceProject.Model.Privilege;
 import com.example.invoiceProject.Service.PrivilegeService;
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/privileges")
+@RequiredArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
+@Slf4j
 public class PrivilegeController {
+    PrivilegeService permissionService;
 
-    @Autowired
-    private PrivilegeService privilegeService;
-
-    @GetMapping("")
-    public List<Privilege> getList(){
-        return privilegeService.getList();
+    @PostMapping
+    ApiResponse<PrivilegeResponse> create(@RequestBody PrivilegeRequest request) {
+        return ApiResponse.<PrivilegeResponse>builder()
+                .result(permissionService.create(request))
+                .build();
     }
 
-    //Add Privilege
-    @PostMapping("/new")
-    public void addPrivilege(@RequestBody Privilege privilege){
-        privilegeService.addPrivilege(privilege);
+    @GetMapping
+    ApiResponse<List<PrivilegeResponse>> getAll() {
+        return ApiResponse.<List<PrivilegeResponse>>builder()
+                .result(permissionService.getList())
+                .build();
     }
 
-    //Delete Privilege
-    @DeleteMapping("/{id}/delete")
-    public void deletePrivilege(@PathVariable Long id){
-        privilegeService.deletePrivilege(id);
+    @DeleteMapping("/{permission}")
+    ApiResponse<Void> delete(@PathVariable Long privilege) {
+        permissionService.delete(privilege);
+        return ApiResponse.<Void>builder().build();
     }
-
 }
