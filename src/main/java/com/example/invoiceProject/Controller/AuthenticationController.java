9f1be515ce8815a9955794282b0ab8a14037e77f
Bluephoenix <biendien4508@gmail.com>
2024-11-22 11:28:08 +0700

    update

--- src/main/java/com/example/invoiceProject/Controller/AuthenticationController.java
+++ src/main/java/com/example/invoiceProject/Controller/AuthenticationController.java
@@ -1,23 +1,34 @@
 package com.example.invoiceProject.Controller;
 
 import com.example.invoiceProject.DTO.requests.IntrospectRequest;
+import com.example.invoiceProject.DTO.requests.MailResetRequest;
 import com.example.invoiceProject.DTO.response.ApiResponse;
 import com.example.invoiceProject.DTO.requests.AuthenticationRequest;
 import com.example.invoiceProject.DTO.response.AuthenticationResponse;
 import com.example.invoiceProject.DTO.response.IntrospectResponse;
+import com.example.invoiceProject.Model.MailDetail;
 import com.example.invoiceProject.Model.User;
 import com.example.invoiceProject.Service.AuthenticateService;
+import com.example.invoiceProject.Service.EmailService;
+import com.example.invoiceProject.Service.UserService;
 import com.nimbusds.jose.JOSEException;
+import lombok.AccessLevel;
+import lombok.experimental.FieldDefaults;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
 import java.text.ParseException;
 
 @RestController
 @RequestMapping("/auth")
+
 public class AuthenticationController {
     @Autowired
     private AuthenticateService authenticationService;
+    private UserService userService;
+    @Autowired
+    private EmailService emailService;
 
 
     @PostMapping("/token")
@@ -35,6 +46,9 @@ public class AuthenticationController {
         return ApiResponse.<IntrospectResponse>builder().result(result).build();
     }
 
+
+
+
 //    @PostMapping("/refresh")
 //    ApiResponse<AuthenticationResponse> authenticate(@RequestBody RefreshRequest request)
 //            throws ParseException, JOSEException {
@@ -47,4 +61,9 @@ public class AuthenticationController {
 //        authenticationService.logout(request);
 //        return ApiResponse.<Void>builder().build();
 //    }
+//        @PostMapping("/sendmail")
+//        public String sendMail(@RequestBody MailDetail mail) {
+//            return emailService.sendSimpleMail(mail);
+//        }
 }
+
