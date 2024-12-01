73589fbf351390f4b522c1f5ffc62117a856cd60
RTRIT <petertringuyen03@gmail.com>
2024-12-01 10:12:42 +0700

    vnpay adjust

---
+++ src/main/java/com/example/invoiceProject/DTO/response/IpnResponse.java
@@ -0,0 +1,2 @@
+package com.example.invoiceProject.DTO.response;public class IpnResponse {
+}
---
+++ src/main/java/com/example/invoiceProject/Service/PaymentService/VnPayIpnHandler.java
@@ -0,0 +1,2 @@
+package com.example.invoiceProject.Service.PaymentService;public class VnPayIpnHandler {
+}
